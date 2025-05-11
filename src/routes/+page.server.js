e3db823c11253aa5d77bf970ea91957b24b1f651
Cristi <cristi@cristi-stan.ro>
2025-05-11 16:05:04 +0300

    added sass package

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
 export async function load() {
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/postsss');
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 
         if (!response.ok) {
             throw new Error('Network response was not ok' + response.statusText);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 
 <script>
-    let { data } = $$props;
-    let { error } = $$props;
-    let { status } = $$props;
+    let { 
+        data,
+        error,
+        status
+    } = $props();
 
     console.log('data: ', data);
     console.log('error: ', error);
@@ -17,3 +19,52 @@
         <p>No data available.</p>
     {/if}
 </main>
+
+<style lang="scss">
+    main {
+        padding: 1rem;
+        background-color: #f0f0f0;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);   
+        
+        h1 {
+            color: var(--primary-color);
+        }
+    }
+</style>
+
+<!-- /**
+add sass to a svelte project
+1. Install the necessary packages:
+   ```bash
+   npm install --save-dev sass svelte-preprocess
+   npm install -D sass-embedded
+   ```
+2. Update your `svelte.config.js` file to include `svelte-preprocess`:
+   ```javascript
+    // svelte.config.js
+    import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+    /** @type {import('@sveltejs/kit').Config} */
+    const config = {
+    preprocess: vitePreprocess()
+    };
+
+    export default config;
+    ```
+3. Use the `lang="scss"` attribute in your Svelte component's `<style>` tag:
+    ```html
+    <style lang="scss">
+         // Your SCSS code here
+    </style>
+    ```
+4. Create a global SCSS file (e.g `app.scss`) in the src directory and import it in your main layout file:
+    ```javascript
+    // src/routes/+layout.svelte
+    <script>
+        import "../app.scss";
+    </script>
+
+    <slot />
+    ```
+*/ -->
